be089a2cdbed7c59b74aca07a75b2d8b21538c66
CJ Avilla <avilla@stainless.com>
2026-05-06 10:54:27 -0400

    Build SDK
    Stainless-Generated-From: 6af9ed1658b5b1b8d55cab322e5ad44fa0797fb6

--- internal/version.go
+++ internal/version.go
@@ -2,4 +2,8 @@
 
 package internal
 
+<<<<<<< HEAD
+const PackageVersion = "0.4.2" // x-release-please-version
+=======
 const PackageVersion = "0.4.0" // x-release-please-version
+>>>>>>> e8c9adf (Apply custom code)
